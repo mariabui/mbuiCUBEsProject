82176b121f591663222a96aa89a63cb729854555
mariabui <mbuiedu@gmail.com>
2023-01-26 22:10:09 -0500

    add functions to get entries data

---
+++ get_entries_functions.py
@@ -0,0 +1,27 @@
+import sys
+import requests
+from secrets import wufoo_key
+from requests.auth import HTTPBasicAuth
+
+
+def get_response(url):
+    response = requests.get(url, auth=HTTPBasicAuth(wufoo_key, 'pass'))
+    if response.status_code != 200:
+        print(f'Failed to get data, response code: {response.status_code} and error message: {response.reason}')
+        sys.exit(-1)
+    return response
+
+
+def get_json_response(url):
+    response = get_response(url)
+    try:
+        return response.json()
+    except requests.exceptions.JSONDecodeError as json_decode_error:
+        print(f'Failed to get data, JSON decode error: {json_decode_error}')
+        sys.exit(-1)
+
+
+def get_entries(url) -> dict:
+    url = f'{url}?pageStart=0&pageSize=100'
+    json_response = get_json_response(url)
+    return json_response['Entries']
--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from get_entries_functions import get_entries
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def main():
+    url = 'https://mbui.wufoo.com/api/v3/forms/cubes-project-proposal-submission/entries/json'
+    entries = get_entries(url)
+    print(entries)
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
