7a386c082b2c5c51225fff3a07014b23f0fc5ba9
mariabui <mbuiedu@gmail.com>
2023-01-31 18:25:27 -0500

    add functions to set up and close db

---
+++ db_handler.py
@@ -0,0 +1,68 @@
+import sys
+import sqlite3
+
+
+def open_db(db_name: str) -> tuple[sqlite3.Connection, sqlite3.Cursor]:
+    try:
+        connection = sqlite3.connect(db_name)
+        cursor = connection.cursor()
+        return connection, cursor
+    except sqlite3.Error as error:
+        print(f'Failed to open database, {error}')
+        sys.exit(-1)
+
+
+def close_db(connection: sqlite3.Connection, cursor: sqlite3.Cursor):
+    try:
+        connection.commit()
+        cursor.close()
+        connection.close()
+    except sqlite3.Error as error:
+        print(f'Failed to close database, {error}')
+        sys.exit(-1)
+
+
+def create_table(cursor: sqlite3.Cursor):
+    try:
+        cursor.execute('''CREATE TABLE IF NOT EXISTS cubes_project_proposal_submissions(
+                       entry_id INTEGER PRIMARY KEY,
+                       prefix TEXT,
+                       first_name TEXT NOT NULL,
+                       last_name TEXT NOT NULL,
+                       title TEXT NOT NULL,
+                       organization_name TEXT NOT NULL,
+                       email TEXT NOT NULL,
+                       organization_website TEXT,
+                       phone_number TEXT,
+                       course_project TEXT,
+                       guest_speaker TEXT,
+                       site_visit TEXT,
+                       job_shadow TEXT,
+                       internships TEXT,
+                       career_panel TEXT,
+                       networking_event TEXT,
+                       summer_2022 TEXT,
+                       fall_2022 TEXT,
+                       spring_2023 TEXT,
+                       summer_2023 TEXT,
+                       other TEXT,
+                       organizations_name_usage TEXT,
+                       date_created TEXT);''')
+    except sqlite3.Error as error:
+        print(f'Failed to create table, {error}')
+        sys.exit(-1)
+
+
+def clear_table(cursor: sqlite3.Cursor):
+    try:
+        cursor.execute('''DELETE FROM cubes_project_proposal_submissions''')
+    except sqlite3.Error as error:
+        print(f'Failed to clear table, {error}')
+        sys.exit(-1)
+
+
+def set_up_db(db_name: str) -> tuple[sqlite3.Connection, sqlite3.Cursor]:
+    connection, cursor = open_db(db_name)
+    create_table(cursor)
+    clear_table(cursor)
+    return connection, cursor
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from api_data import get_entries, save_entries_to_text_file
+from db_handler import set_up_db, close_db
 
 
 def main():
@@ -7,6 +8,9 @@ def main():
 
     filename = 'cubes_project_proposal_submissions.txt'
     save_entries_to_text_file(entries, filename)
+
+    connection, cursor = set_up_db('cubes_project_proposal_submissions.sqlite')
+    close_db(connection, cursor)
 
 
 if __name__ == '__main__':
