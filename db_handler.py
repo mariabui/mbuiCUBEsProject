419b1776a0ae90192444ba8e0bd11336a2940dca
mariabui <mbuiedu@gmail.com>
2023-02-01 16:12:08 -0500

    refactor convert_entries_to_list_of_tuples function

--- db_handler.py
+++ db_handler.py
@@ -46,7 +46,7 @@ def create_table(cursor: sqlite3.Cursor):
                        spring_2023 TEXT,
                        summer_2023 TEXT,
                        other TEXT,
-                       organizations_name_usage TEXT,
+                       organization_name_usage TEXT,
                        date_created TEXT);''')
     except sqlite3.Error as error:
         print(f'Failed to create table, {error}')
@@ -71,30 +71,13 @@ def set_up_db(db_name: str) -> tuple[sqlite3.Connection, sqlite3.Cursor]:
 def convert_entries_to_list_of_tuples(entries: list[dict]) -> list[tuple]:
     entries_list_of_tuples = []
     for entry in entries:
-        entry_data = (entry['EntryId'],
-                      None if entry['Field1'] == '' else entry['Field1'],
-                      entry['Field2'],
-                      entry['Field3'],
-                      entry['Field4'],
-                      entry['Field5'],
-                      entry['Field6'],
-                      None if entry['Field7'] == '' else entry['Field7'],
-                      None if entry['Field8'] == '' else entry['Field8'],
-                      'No' if entry['Field9'] == '' else 'Yes',
-                      'No' if entry['Field10'] == '' else 'Yes',
-                      'No' if entry['Field11'] == '' else 'Yes',
-                      'No' if entry['Field12'] == '' else 'Yes',
-                      'No' if entry['Field13'] == '' else 'Yes',
-                      'No' if entry['Field14'] == '' else 'Yes',
-                      'No' if entry['Field15'] == '' else 'Yes',
-                      'No' if entry['Field109'] == '' else 'Yes',
-                      'No' if entry['Field110'] == '' else 'Yes',
-                      'No' if entry['Field111'] == '' else 'Yes',
-                      'No' if entry['Field112'] == '' else 'Yes',
-                      'No' if entry['Field113'] == '' else 'Yes',
-                      entry['Field210'],
-                      entry['DateCreated'])
-        entries_list_of_tuples.append(entry_data)
+        entry_values = list(entry.values())[:23]
+        entry_values[0] = int(entry_values[0])
+        for field in [1, 7, 8]:
+            entry_values[field] = None if entry_values[field] == '' else entry_values[field]
+        for field in range(9, 21):
+            entry_values[field] = 'No' if entry_values[field] == '' else 'Yes'
+        entries_list_of_tuples.append(tuple(entry_values))
     return entries_list_of_tuples
 
 
@@ -102,8 +85,7 @@ def save_entries_to_db(entries: list[dict], cursor: sqlite3.Cursor):
     entries_list_of_tuples = convert_entries_to_list_of_tuples(entries)
     try:
         cursor.executemany('''INSERT INTO entries VALUES(
-        ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''',
-                           entries_list_of_tuples)
+        ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''', entries_list_of_tuples)
     except sqlite3.Error as error:
         print(f'Failed to save entries to database, {error}')
         sys.exit(-1)
